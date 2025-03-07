b9c2f32712b70166ec826b44b560434eeb28d129
Lalance67 <Lancenathandebelen@gmail.com>
2025-03-07 22:21:56 +0800

    Add files via upload

---
+++ 006_variables/variables.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+    int age = 18;
+    char name[] = "Lance";
+    char grade = 'C';
+    float average = 2.05;
+
+    printf("You are %d\n", age);
+    printf("Your name is %s\n", name);
+    printf("your grade is %c\n", grade);
+    printf("you have an average grade of %.2f\n", average);
+
+    return 0;
+}
+
+
+//
+/*
+int main()
+{
+    int age = 18;
+    char name[] = "lance";
+    char grade = 'C';
+    float gpa = 2.05;
+    printf("hello, %s\n", name);
+    printf("you are %d years old\n",age);
+    printf("your average grade is %c\n", grade);
+    printf("your gpa is %f\n", gpa);
+    return 0;
+}
+*/
+//
