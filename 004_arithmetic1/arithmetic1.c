4247af3d2e0bfd4fbed1dd4361a4430aeb391da2
Lalance67 <Lancenathandebelen@gmail.com>
2025-03-07 22:18:38 +0800

    Add files via upload

---
+++ 004_arithmetic1/arithmetic1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int x = 1;
+    int y = 5;
+
+    float z = (float) x / y;
+
+    printf("x divided by y is equal to: %f", z);
+}
+
+// 
+/* int main()
+{
+int x = 2;
+int y = 5;
+
+float  z = (float)x / y;
+printf("%f", z);
+return 0;
+} */
+//
