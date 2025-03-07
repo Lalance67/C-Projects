748e2146a7b8a837f9f4e5b83bb0acca6527871b
Lalance67 <Lancenathandebelen@gmail.com>
2025-03-07 22:31:26 +0800

    Add files via upload

---
+++ 009_hypotenuse/hypotenuse.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    double A;
+    double B;
+    double C;
+
+    printf("Enter side A:\n");
+    scanf("%lf", &A);
+
+    printf("Enter side B:\n", &B);
+    scanf("%lf", &B);
+
+    C = sqrt(A*A + B*B);
+
+    printf("Side C: %.2lf", C);
+
+    return 0;
+}
+//
+/*
+int main()
+{
+    double A;
+    double B;
+    double C;
+
+    printf("Enter side A:\n");
+    scanf("%lf", &A);
+
+    printf("Enter side B:\n");
+    scanf("%lf", &B);
+
+    C = sqrt(A*A + B*B);
+
+    printf("Side C:%lf", C);
+
+    return 0;
+}
+*/
+//
