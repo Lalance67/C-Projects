31b8c4205ae302af62bbccb32c8600c8fec227b3
Lalance67 <Lancenathandebelen@gmail.com>
2025-03-07 22:20:31 +0800

    Add files via upload

---
+++ 005_specifier/specifier.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+float item1 = 5.75;
+float item2 = 10.00;
+float item3 = 100.99;
+
+printf("item 1: $%.2f\n", item1);
+printf("item 2: $%.2f\n", item2);
+printf("item 3: $%.2f\n", item3);
+return 0;
+}
